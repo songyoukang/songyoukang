574f13f36641969a4c326c1e7467056e1857e8a9
songyoukang <syk199409@163.com>
2022-08-18 00:38:31 +0800

    test61311111

---
+++ html_02.py
@@ -0,0 +1,29 @@
+
+
+# def table():
+#     """九九乘法表"""
+#     num=1
+#     while num<=9:
+#         col = 1
+#         while col<=num:
+#             print("%d*%d=%d"% (col,num,num*col),end=" \t")
+#             # print("%d*%d=num*col",end=" ")
+#             col+=1
+#         #print("第%d行"%num)
+#         print("")
+#         num+=1
+#
+# def num(num1,num2):
+#     # num1=1
+#     # num2=2
+#
+#     result=num1+num2
+#     print ("%d+%d=%d"%(num1,num2,result))
+#     table()
+# table()
+# num(2,2)
+# # num_result=num(2,1)
+# # print(num_result)
+
+def html(a,b):
+    print(a*b)
